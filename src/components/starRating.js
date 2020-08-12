2ddac2e613a00ae9bc17cc68ab2658d5223845ca
michela iacobucci <michelaiacobucci@MacBook-Air.local>
2020-08-12 11:48:58 -0700

    starclasses

--- src/components/starRating.js
+++ src/components/starRating.js
@@ -1,29 +1,35 @@
 import StarRatings from './react-star-ratings'
 
-export default function Star() {
-    function changeRating( newRating, name) {
+class Star extends Component {
+    changeRating(newRating, name) {
         this.setState({
             rating: newRating
         })
     }
-    return() {
-        <StarRatings
-        rating={this.state.rating}
-        starRatedColor="gold"
-        starEmptyColor="grey"
-        changeRating={this.changeRating}
-        numberOfStars={5}
-        name='rating'
-        />
+    render() {
+        return (
+            <StarRatings
+                rating={this.state.rating}
+                starRatedColor="gold"
+                starEmptyColor="grey"
+                changeRating={this.changeRating}
+                numberOfStars={5}
+                name='rating'
+            />
+
+        )
+
     }
 }
 
-export default function Rating() {
-    return (
-        <StarRatings
-        rating={2.403}
-        starDimension="30px"
-        starSpacing="15px"
-        />
-    )
+class Rating extends Component {
+    render() {
+        return (
+            <StarRatings
+                rating={2.403}
+                starDimension="30px"
+                starSpacing="15px"
+            />
+        )
+    }
 }
