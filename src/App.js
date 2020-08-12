eab242a9233e960ddf8063bbc32fa72819ae19e7
d4vves <d4vves@gmail.com>
2020-08-12 13:42:27 -0500

    button components created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LikeButton from './components/LikeButton'
+import DislikeButton from './components/DislikeButton'
+import MoreInfoButton from './components/MoreInfoButton'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LikeButton />
+      <DislikeButton />
+      <MoreInfoButton />
     </div>
   );
 }
---
+++ src/components/DislikeButton.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DislikeButton() {
+    return (
+        <svg height={30} viewBox="0 0 21 21" width={30} xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd" stroke="#2a2e3b" strokeLinecap="round" strokeLinejoin="round" transform="translate(2 2)"><circle cx="8.5" cy="8.5" r={8} /><g transform="matrix(0 1 -1 0 17 0)"><path d="m5.5 11.5 6-6" /><path d="m5.5 5.5 6 6" /></g></g></svg>
+    )
+}
