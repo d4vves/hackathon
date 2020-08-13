de85caa849753b0c27588c38606725e4c0763c0a
michela iacobucci <miacobucci17@gmail.com>
2020-08-12 21:32:47 -0700

    figma rep

--- src/components/Landmark.js
+++ src/components/Landmark.js
@@ -12,12 +12,20 @@ export default function Landmark(props) {
     return (
         <div>
             <div className="background">
-                <img width='500px' src={props.currentLandmark.image} />
+                <img width='500px' height='600' src={props.currentLandmark.image} alt="landmark" />
             </div>
             <div className={`${showInfo ? 'info' : 'hideInfo'}`}>
                 <div className>
                     <span>{props.currentLandmark.name}</span>
                     <span>{props.currentLandmark.city}, {props.currentLandmark.country}</span>
+                </div>
+                <div>
+                    <span>{props.currentLandmark.language}</span>
+                    <span>{props.currentLandmark.currency}</span>
+                    <span>{props.currentLandmark.climate}</span>
+                </div>
+                <div>
+                    <img width="200px" height="100px" src={props.currentLandmark.location} alt="landmark-location"/>
                 </div>
                 <div>
                     <span>{props.currentLandmark.desc}</span>
