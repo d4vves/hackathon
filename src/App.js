8372cb8fd1b1d4b6d21f56065d86cb7a4d059c4d
d4vves <d4vves@gmail.com>
2020-08-12 19:06:25 -0500

    navigation buttons functioning

--- src/App.js
+++ src/App.js
@@ -1,65 +1,36 @@
 import React, { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import './App.css';
-// import Header from './components/Header';
-import Picture from './components/Picture';
-import StarRating from './components/starRating'
+import Buttons from './components/Buttons'
 
 import data from './data.json';
 
 const App = () => {
-  const [picture, setPicture] = useState(data);
-  const [likedPictures, setLikedUsers] = useState([]);
-  const [dislikedPictures, setDislikedPictures] = useState([]);
-  const activePicture = 0;
+  let [index, setIndex] = useState(0)
+  let [currentLandmark, setCurrentLandmark] = useState(data[index])
+  let [favoriteLandmarks, setFavoriteLandmarks] = useState([])
 
-  const removedPersonFromDataSrc = (pictureSource, userId) =>
-    pictureSource.filter(user => user.id !== userId);
-
-  const modifySuperficialChoices = (userId, action) => {
-    const newPicture = [...picture];
-    const newLikedPictures = [...likedPictures];
-    
-    const newDislikedPictures = [...dislikedPictures];
-
-    switch (action) {
-      case 'ADD_TO_LIKED_PICTURES':
-        if (!picture[activePicture].likedUsers.includes(userId)) {
-          newPicture[activePicture].likedUsers.push(userId);
-          newLikedPictures.push(data[userId]);
-
-          setLikedUsers(newLikedPictures);
-          setPicture(removedPersonFromDataSrc(picture, userId));
-        }
-        break;
-      case 'ADD_TO_DISLIKED_PICTURE':
-        if (!picture[activePicture].dislikedPictures.includes(userId)) {
-          newPicture[activePicture].dislikedPictures.push(userId);
-          newDislikedPictures.push(data[userId]);
-
-          setDislikedPictures(newLikedPictures);
-          setPicture(removedPersonFromDataSrc(picture, userId));
-        }
-        break;
-      
-      default:
-        return picture;
-    }
-  };
+  const swapLandmark = () => {
+    setIndex(index += 1)
+    console.log(index)
+    setCurrentLandmark(data[index])
+  }
+  
+  const addLandmark = () => {
+    let newFavoriteLandmarks = [currentLandmark, ...favoriteLandmarks]
+    console.log(newFavoriteLandmarks)
+    setFavoriteLandmarks(newFavoriteLandmarks)
+    swapLandmark()
+  }
 
   return (
-    <div className="app">
-      {/* <Header /> */}
-      {picture[1] ? (
-        <Picture
-          key={picture[1].id}
-          picture={picture[1]}
-          modifySuperficialChoices={modifySuperficialChoices}
-          likedPicture={likedPictures}
-        />
-      ) : (
-        <> </>
-      )}
-    </div>
+    <>
+      <BrowserRouter>
+        <p>NAVBAR COMPONENT</p>
+        <img src={currentLandmark.image} width='500px' />
+        <Buttons addLandmark={addLandmark} swapLandmark={swapLandmark} />
+      </BrowserRouter>
+    </>
   );
 };
 
--- src/components/svg/DislikeButton.js
+++ src/components/svg/DislikeButton.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-export default function DislikeButton() {
+export default function DislikeButton(props) {
     return (
-        <svg height={30} viewBox="0 0 21 21" width={30} xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd" stroke="#2a2e3b" strokeLinecap="round" strokeLinejoin="round" transform="translate(2 2)"><circle cx="8.5" cy="8.5" r={8} /><g transform="matrix(0 1 -1 0 17 0)"><path d="m5.5 11.5 6-6" /><path d="m5.5 5.5 6 6" /></g></g></svg>
+        <button className='dislike-button' onClick={props.swapLandmark}>
+            <svg height={30} viewBox="0 0 21 21" width={30} xmlns="http://www.w3.org/2000/svg"><g fill="none" fillRule="evenodd" stroke="#248BC5" strokeLinecap="round" strokeLinejoin="round" transform="translate(5 5)"><path d="m.5 10.5 10-10" /><path d="m10.5 10.5-10-10z" /></g></svg>
+        </button>
     )
 }
